30d76eb82c30105251bb03554bd6c99819088a67
Bagus Nur A <bagusnur@protonmail.com>
2023-01-24 14:38:02 +0800

    Feat: few dashboard items

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $todayStart = Carbon::today();
+        $todayEnd = (new Carbon($todayStart))->endOfDay();
+
+        $todayTicket = Ticket::whereBetween('created_at', [$todayStart, $todayEnd]);
+        $todayTicketCount = $todayTicket->count();
+
+        $todayActiveTicketCount = $todayTicket->where('status', 'Aktif')->count();
+        $todayDoneTicketCount = $todayTicket->where('status', 'Selesai')->count();
+        
         return view('dashboard', [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'todayTicketCount' => $todayTicketCount,
+            'todayActiveTicketCount' => $todayActiveTicketCount,
+            'todayDoneTicketCount' => $todayDoneTicketCount,
         ]);
     }
 }
